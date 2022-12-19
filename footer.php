c29445c0e51581e04dcd21337ac1deb8486c6d04
Nastin Gwaza <114344748+Nastin2022@users.noreply.github.com>
2022-12-19 18:20:59 +0800

    Uploaded files from computer

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div> <!-- /.container -->
+<footer class="blog-footer">
+      <p>Custom Theme Built for <a href="https://petruthit.com/">Nastin Mfena</a> by <a href="https://petruthit.com/">@nastinterry</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>PurPle Bug Technical Exam</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
+  
+  <?php wp_head();?>
+</head>
+
+<body>
+
+ <div class="blog-masthead">
+    <div class="container">
+      <nav class="blog-nav">
+        <a class="blog-nav-item active" href="https://purplebug.petruthcloset.com/">Home</a>
+        <a class="blog-nav-item" href="#">Shopify</a>
+        <a class="blog-nav-item" href="#">WordPress</a>
+        <a class="blog-nav-item" href="#">New hires</a>
+        <a class="blog-nav-item" href="https://purplebug.petruthcloset.com/about-us/">About Purple Bug</a>
+      </nav>
+    </div>
+  </div>
+
+  <div class="container">
+
+    <div class="blog-header">
+  <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+  <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+   </div>
